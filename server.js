d9817cb60dad12c2917cde96d4d78f46a257db71
Dave Bokil <bokild@gmail.com>
2017-05-29 23:58:12 -0400

    hello watson

--- server.js
+++ server.js
@@ -44,6 +44,55 @@ app.use("/", routes);
 app.use("/update", routes);
 app.use("/create", routes);
 
+// Watson Code
+//=========================================================
+require('dotenv').load({ silent: true });
+
+// on bluemix, enable rate-limiting and force https
+if (process.env.VCAP_SERVICES) {
+  // enable rate-limiting
+  const RateLimit = require('express-rate-limit');
+  app.enable('trust proxy'); // required to work properly behind Bluemix's reverse proxy
+
+  const limiter = new RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    delayMs: 0 // disable delaying - full speed until the max limit is reached
+  });
+
+  //  apply to /api/*
+  app.use('/api/', limiter);
+
+  // force https - microphone access requires https in Chrome and possibly other browsers
+  // (*.mybluemix.net domains all have built-in https support)
+  const secure = require('express-secure-only');
+  app.use(secure());
+}
+
+// token endpoints
+// **Warning**: these endpoints should probably be guarded with additional authentication & authorization for production use
+app.use('/api/speech-to-text/', require('./stt-token.js'));
+app.use('/api/text-to-speech/', require('./tts-token.js'));
+
+// chrome requires https to access the user's microphone unless it's a localhost url so
+// this sets up a basic server at https://localhost3001/ using an included self-signed certificate
+// note: this is not suitable for production use
+// however bluemix automatically adds https support at http://<myapp>.mybluemix.net
+if (!process.env.VCAP_SERVICES) {
+  const fs = require('fs');
+  const https = require('https');
+  const HTTPS_PORT = 3001;
+
+  const options = {
+    key: fs.readFileSync(__dirname + '/keys/localhost.pem'),
+    cert: fs.readFileSync(__dirname + '/keys/localhost.cert')
+  };
+  https.createServer(options, app).listen(HTTPS_PORT, function() {
+    console.log('Secure server live at https://localhost:%s/', HTTPS_PORT);
+  });
+}
+
+
 
 // listen on port 3000
 var port = process.env.PORT || 3000;
