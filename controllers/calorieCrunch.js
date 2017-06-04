97594ea15dc09afa5c98d9c1c756d136f54f16cb
himapratap <himapratap@gmail.com>
2017-06-04 17:01:49 -0400

    Changed user profile , changed time

--- controllers/calorieCrunch.js
+++ controllers/calorieCrunch.js
@@ -8,7 +8,7 @@
 var express = require("express");
 var passport = require("../config/passport");
 var request = require("request")
-
+var moment = require("moment");
 
 var router = express.Router();
 // grabbing our models
@@ -38,34 +38,81 @@ router.post("/verifyLogin", passport.authenticate("local", {
     failureRedirect: '/login', // redirect back to the signup page if there is an error
 }));
 
+function loadUserProfile(req, res) {
+    var date = moment().format("YYYY-MM-DD");
+    console.log("Current time", date);
+    db.Activity.findAll({
+        where: {
+            UserId: req.session.passport.user.id,
+            time: date
+        },
+
+        raw: true
+
+    }).then(function(results) {
+        console.log(results);
+        var totalCal = 0;
+        results.map((x) => {
+            totalCal += x.totalCalories
+            x.time = moment(x.createdAt).format("hh:mm a");
+
+         })
+
+        let userProfile = {
+            'entry': results,
+            'totalCal': totalCal,
+            'user': req.session.passport.user,
+            'searchResults': req.searchResults
+        }
+        //console.log("userProfile.searchResults", userProfile.searchResults);
+
+
+        res.render("user", {
+            'userProfile': userProfile
+        });
+
+    });
+}
+
+
+function searchFood(req, res, next) {
+
+    let item = req.body.foodsearch;
+    let appID = "48e9aea9";
+    let appKey = "3cfd31e974a75dc9c2b9cc64a1b40dd6";
+    let url = "https://api.nutritionix.com/v1_1/search/" + item + "?results=0:10&fields=item_name,nf_servings_per_container,nf_cholestorol,nf_saturated_fat,nf_sodium,nf_total_carbohydrate,nf_dietary_fiber,nf_sugars,nf_protein,nf_serving_size_qty,nf_total_fat,brand_name,item_id,nf_calories&appId=" + appID + "&appKey=" + appKey;
+    request(url, function(error, response, body) {
+
+        // If there were no errors and the response code was 200 (i.e. the request was successful)...
+        if (!error && response.statusCode === 200) {
+            body = JSON.parse(body);
+            let data = body.hits;
+            let items = data.map((x) => {
+                return x.fields;
+            })
+            let searchResults = {
+                'items': items,
+                showTitle: true,
+                dosearch: true
+
+            }
+            //  console.log('Retrieved items after searching!',searchResults);
+
+            req.searchResults = searchResults;
+            return next();
+            // res.render(req.body.page, {
+            //     'items': items,
+            //     showTitle: true,
+            //     dosearch: true
+            // });
+        }
+    });
+
+}
+
 router.get("/user", isAuthenticated, function(req, res) {
-  db.Activity.findAll({
-      where: {
-          UserId: req.session.passport.user.id,
-          time: db.sequelize.literal('CURRENT_TIMESTAMP')
-      },
-
-      raw: true
-
-  }).then(function(results) {
-      console.log(results);
-      var totalCal = 0;
-      results.map((x) => {
-          totalCal += x.totalCalories
-      })
-
-      let userProfile = {
-          'entry': results,
-          'totalCal': totalCal,
-          'user'  :  req.session.passport.user
-      }
-      console.log("userProfile", userProfile);
-      res.render("user", {
-          'userProfile': userProfile
-      });
-
-  });
- });
+    loadUserProfile(req, res);
+});
 
 router.post("/signup", function(req, res) {
     console.log(`Starting signup process : }`);
@@ -102,41 +149,17 @@ router.post("/signup", function(req, res) {
 });
 
 
-router.post("/searchfood", function(req, res) {
-    let item = req.body.foodsearch;
-    let appID = "48e9aea9";
-    let appKey = "3cfd31e974a75dc9c2b9cc64a1b40dd6";
-    let url = "https://api.nutritionix.com/v1_1/search/" + item + "?results=0:10&fields=item_name,nf_servings_per_container,nf_cholestorol,nf_saturated_fat,nf_sodium,nf_total_carbohydrate,nf_dietary_fiber,nf_sugars,nf_protein,nf_serving_size_qty,nf_total_fat,brand_name,item_id,nf_calories&appId=" + appID + "&appKey=" + appKey;
-    request(url, function(error, response, body) {
-
-        // If there were no errors and the response code was 200 (i.e. the request was successful)...
-        if (!error && response.statusCode === 200) {
-            body = JSON.parse(body);
-            let data = body.hits;
-            let items = data.map((x) => {
-                return x.fields;
-            })
-            console.log('Retrieved items after searching!');
-            // console.log(data)
-            //      console.log(items)
-            // res.render(req.body.page, {'items' :items});
-            res.render(req.body.page, {
-                'items': items,
-                showTitle: true,
-                dosearch: true
-            });
-        }
-    });
-
-});
+router.post("/searchfood", searchFood, loadUserProfile);
 
 router.post("/addFood", (req, res) => {
+  var date = moment().format("YYYY-MM-DD");
+
     console.log(req.session);
     console.log("adding food", req.body);
     let activity = {
         food: req.body.food,
         quantity: req.body.quantity,
-        time: db.sequelize.literal('CURRENT_TIMESTAMP'),
+        time:date,
         UserId: req.session.passport.user.id,
         calories: req.body.calories
     };
@@ -146,7 +169,7 @@ router.post("/addFood", (req, res) => {
         raw: true
     }).then(function() {
 
-             res.redirect("/user");
+            res.redirect("/user");
 
 
         }
--- models/activity.js
+++ models/activity.js
@@ -1,12 +1,15 @@
 // Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
 // sometimes causes errors on Windows machines
-
+var moment = require('moment');
 // Creating our Activity model
 module.exports = function(sequelize, DataTypes) {
     var Activity = sequelize.define("Activity", {
             // The email cannot be null, and must be a proper email before creation
             time: {
                 type: DataTypes.DATEONLY,
+                get: function() {
+                       return moment.utc(this.getDataValue('regDate')).format('YYYY-MM-DD');
+                     }
                 //  allowNull: false
             },
             food: {
