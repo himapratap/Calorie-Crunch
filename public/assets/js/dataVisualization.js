82308874de7014381623262a5df6d5c13c1bcd89
himapratap <himapratap@gmail.com>
2017-06-04 21:40:18 -0400

    total cal per week updated for data visualisation

--- public/assets/js/dataVisualization.js
+++ public/assets/js/dataVisualization.js
@@ -1,100 +1,105 @@
-//****************************************************
-//Progress Bar
+$(document).ready(function() {
 
 
-    let totalCals = $("#totalCalories").val();
-    let bar = new ProgressBar.Line(progressBar, {
-        strokeWidth: 4,
-        easing: 'easeInOut',
-        duration: 1500,
-        color: '#FFEA82',
-        trailColor: '#eee',
-        trailWidth: 1,
-        svgStyle: {width: '100%', height: '100%'},
+  //****************************************************
+  //Progress Bar
 
-        from: {color: '#00ffcc'},
-        to: {color: '#00ffcc'},
-        step: (state, bar) => {
-            bar.path.setAttribute('stroke', state.color);
 
-        }
-    });
+      let totalCals = $("#totalCalories").text();
+      let bar = new ProgressBar.Line(progressBar, {
+          strokeWidth: 4,
+          easing: 'easeInOut',
+          duration: 1500,
+          color: '#FFEA82',
+          trailColor: '#eee',
+          trailWidth: 1,
+          svgStyle: {width: '100%', height: '100%'},
 
-    bar.animate(12/14);  // Number from 0.0 to 1.0"
+          from: {color: '#00ffcc'},
+          to: {color: '#00ffcc'},
+          step: (state, bar) => {
+              bar.path.setAttribute('stroke', state.color);
 
-//****************************************************
+          }
+      });
 
-//****************************************************
-// Graph
+      bar.animate(12/14);  // Number from 0.0 to 1.0"
 
-Highcharts.chart('graphy', {
-    chart: {
-        backgroundColor: '#505393',
-        polar: true,
-        type: 'line',
-        // width: 500,
-        style: {
-            fontFamily: "Open Sans",
-        },
+  //****************************************************
 
-    },
-    title: {
-        text: "",
-        color:'#ffffff',
-    },
-    xAxis: {
-        categories: ['-6 days', '-5', '-4', '-3','-2','-1' ,'Current'],
-        labels: {
-            style: {
-                color: "#ffffff",
-                font: "Open Sans"
-            }
-        },
-        title: {
-            style: {
-                color: "#ffffff",
-                fontSize: "12px",
-                fontWeight: "bold",
-                fontFamily: "Open Sans"
-            }
-        }
-    },
-    yAxis: {
-        labels: {
-            style: {
-                color: "#ffffff",
-                font: "fontAwesome"
-            }
-        },
-        title: {
-            text: "Calories",
-            style: {
-                color: "#ffffff",
-                fontSize: "12px",
-                // fontWeight: "bold",
-                fontFamily: "Open Sans"
-            }
-        }
-    },
-    labels: {
+  //****************************************************
+  // Graph
 
-    },
-    series: [{
+  Highcharts.chart('graphy', {
+      chart: {
+          backgroundColor: '#505393',
+          polar: true,
+          type: 'line',
+          // width: 500,
+          style: {
+              fontFamily: "Open Sans",
+          },
 
-        type: 'column',
-        name: 'Calories Eaten',
-        data: [1400, 1419, 1650, 1419, 1500, 1419, 1300],
-        color:'#21b2a6',
-    }, {
-        type: 'spline',
-        name: 'Target Calorie',
-        data: [1419, 1419, 1419,1419, 1419, 1419, 1319],
-        color:'#ed4933',
-        marker: {
-            lineWidth: 2,
-            lineColor: Highcharts.getOptions().colors[3],
-            fillColor: 'white'
-        }
+      },
+      title: {
+          text: "",
+          color:'#ffffff',
+      },
+      xAxis: {
+          categories: ['-6 days', '-5', '-4', '-3','-2','-1' ,'Current'],
+          labels: {
+              style: {
+                  color: "#ffffff",
+                  font: "Open Sans"
+              }
+          },
+          title: {
+              style: {
+                  color: "#ffffff",
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                  fontFamily: "Open Sans"
+              }
+          }
+      },
+      yAxis: {
+          labels: {
+              style: {
+                  color: "#ffffff",
+                  font: "fontAwesome"
+              }
+          },
+          title: {
+              text: "Calories",
+              style: {
+                  color: "#ffffff",
+                  fontSize: "12px",
+                  // fontWeight: "bold",
+                  fontFamily: "Open Sans"
+              }
+          }
+      },
+      labels: {
 
-    }]
+      },
+      series: [{
+
+          type: 'column',
+          name: 'Calories Eaten',
+          data: [1400, 1419, 1650, 1419, 1500, 1419, 1300],
+          color:'#21b2a6',
+      }, {
+          type: 'spline',
+          name: 'Target Calorie',
+          data: [1419, 1419, 1419,1419, 1419, 1419, 1319],
+          color:'#ed4933',
+          marker: {
+              lineWidth: 2,
+              lineColor: Highcharts.getOptions().colors[3],
+              fillColor: 'white'
+          }
+
+      }]
+  });
+
 });
