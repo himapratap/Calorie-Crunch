692c2090d746c67a8ce8789304cf764832316301
himapratap <himapratap@gmail.com>
2017-06-05 22:36:54 -0400

    graph done

--- public/assets/js/dataVisualization.js
+++ public/assets/js/dataVisualization.js
@@ -1,11 +1,10 @@
-
 $(document).ready(function() {
 
 
     //****************************************************
     //Progress Bar
 
-    let caloriesPerDay = $("#caloriesPerDay").val();
+    let caloriesPerDay = $(".caloriesPerDay").html().split(':')[1];;
     let caloriesRequired = $("#caloriesRequired").text();
     console.log(caloriesRequired)
     console.log(caloriesPerDay)
@@ -16,93 +15,118 @@ $(document).ready(function() {
         color: '#FFEA82',
         trailColor: '#eee',
         trailWidth: 1,
-        svgStyle: {width: '100%', height: '100%'},
+        svgStyle: {
+            width: '100%',
+            height: '100%'
+        },
 
-        from: {color: '#00ffcc'},
-        to: {color: '#00ffcc'},
+        from: {
+            color: '#00ffcc'
+        },
+        to: {
+            color: '#00ffcc'
+        },
         step: (state, bar) => {
             bar.path.setAttribute('stroke', state.color);
 
         }
     });
 
-    bar.animate(12/14);  // Number from 0.0 to 1.0"
+    bar.animate(12 / 14); // Number from 0.0 to 1.0"
 
     //****************************************************
 
     //****************************************************
     // Graph
+    $("#history").click(function() {
+        var days = $("#daysInAWeek").data('days-data');
+        var xDays = days.split(",");
+        console.log(xDays);
 
-    Highcharts.chart('graphy', {
-        chart: {
-            backgroundColor: '#505393',
-            polar: true,
-            type: 'line',
-            // width: 500,
-            style: {
-                fontFamily: "Open Sans",
-            },
+        var cals = $("#calsInAWeek").data('cals-data');
+        var calsEatenStringArr = cals.split(",");
+        var calsEatenArr = calsEatenStringArr.map(function(x) {
+            return parseInt(x, 10);
+        });
 
-        },
-        title: {
-            text: "",
-            color:'#ffffff',
-        },
-        xAxis: {
-            categories: ['-6 days', '-5', '-4', '-3','-2','-1' ,'Current'],
-            labels: {
+        var calRequired = [];
+        calsEatenArr.map(x => calRequired.push(parseInt(caloriesRequired)))
+        console.log(`calRequired ${calRequired}}`); 
+        Highcharts.chart('graphy', {
+            chart: {
+                backgroundColor: '#505393',
+                polar: true,
+                type: 'line',
+                // width: 500,
                 style: {
-                    color: "#ffffff",
-                    font: "Open Sans"
-                }
-            },
-            title: {
-                style: {
-                    color: "#ffffff",
-                    fontSize: "12px",
-                    fontWeight: "bold",
-                    fontFamily: "Open Sans"
-                }
-            }
-        },
-        yAxis: {
-            labels: {
-                style: {
-                    color: "#ffffff",
-                    font: "fontAwesome"
-                }
+                    fontFamily: "Open Sans",
+                },
+
             },
             title: {
-                text: "Calories",
-                style: {
-                    color: "#ffffff",
-                    fontSize: "12px",
-                    // fontWeight: "bold",
-                    fontFamily: "Open Sans"
+                text: "",
+                color: '#ffffff',
+            },
+            xAxis: {
+                categories: xDays,
+                labels: {
+                    style: {
+                        color: "#ffffff",
+                        font: "Open Sans"
+                    }
+                },
+                title: {
+                    style: {
+                        color: "#ffffff",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        fontFamily: "Open Sans"
+                    }
                 }
-            }
-        },
-        labels: {
+            },
+            yAxis: {
+                labels: {
+                    style: {
+                        color: "#ffffff",
+                        font: "fontAwesome"
+                    }
+                },
+                title: {
+                    text: "Calories",
+                    style: {
+                        color: "#ffffff",
+                        fontSize: "12px",
+                        // fontWeight: "bold",
+                        fontFamily: "Open Sans"
+                    }
+                }
+            },
+            labels: {
 
-        },
-        series: [{
+            },
+            series: [{
 
-            type: 'column',
-            name: 'Calories Eaten',
-            data: [1400, 1419, 1650, 1419, 1500, 1419, 1300],
-            color:'#21b2a6',
-        }, {
-            type: 'spline',
-            name: 'Target Calorie',
-            data: [1419, 1419, 1419,1419, 1419, 1419, 1319],
-            color:'#ed4933',
-            marker: {
-                lineWidth: 2,
-                lineColor: Highcharts.getOptions().colors[3],
-                fillColor: 'white'
-            }
+                type: 'column',
+                name: 'Calories Eaten',
+                data: calsEatenArr,
+                color: '#21b2a6',
+            }, {
+                type: 'spline',
+                name: 'Target Calorie',
+                data: calRequired,
+                color: '#ed4933',
+                marker: {
+                    lineWidth: 2,
+                    lineColor: Highcharts.getOptions().colors[3],
+                    fillColor: 'white'
+                }
 
-        }]
-    });
+            }]
+        });
+
+    })
+
+
+
 
 });
